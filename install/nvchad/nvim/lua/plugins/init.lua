eab813fa9debe93638aa46e912dca4e9eca3c92f
TermuxDev <termuxdev.00@gmail.com>
2024-04-11 23:23:44 -0500

    Fixed some bugs

---
+++ install/nvchad/nvim/lua/plugins/init.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+ {
+   "neovim/nvim-lspconfig",
+   config = function()
+     require("nvchad.configs.lspconfig").defaults()
+     require "configs.lspconfig"
+   end,
+ },
+
+ {
+ 	"williamboman/mason.nvim",
+ 	opts = {
+ 		ensure_installed = {
+ 			"html-lsp", "css-lsp" , "prettier", "tailwindcss-language-server", "typescript-language-server"
+ 		},
+ 	},
+ },
+
+ {
+ 	"nvim-treesitter/nvim-treesitter",
+ 	opts = {
+ 		ensure_installed = {
+ 			"vim", "lua", "vimdoc",
+      "html", "css", "javascript", "typescript", "bash", "json", "tsx", "gitignore"
+ 		},
+ 	},
+ },
+}
